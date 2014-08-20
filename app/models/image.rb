f07a66f4e79ef878d2e6201559e50e0fe279898d
Richard Kent Jordan <rjordan@pobox.com>
2014-08-20 00:03:12 -0500

    Begin refactoring access to Docker::Images though model class

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,21 @@
-class Image < ActiveRecord::Base
+class Image
+  attr_reader :id
+
+  def self.all
+    Docker::Image.all
+  end
+
+  def self.find(id)
+    image = Docker::Image.get(id)
+    create_from_docker(image)
+  end
+
+  private
+
+  def self.create_from_docker(image_data)
+    image = Image.new
+    image.instance_variable_set('@id', image_data.id)
+    image
+  end
+
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Image, :type => :model do
+  describe '#all' do
+    before do
+      @response = Image.all
+    end
+    it { expect(@response).to be_kind_of(Array) }
+  end
+
+  describe '#find' do
+    before do
+      data = double(id: 1)
+      expect(Docker::Image).to receive(:get).with(1).and_return(data)
+      @image = Image.find(1)
+    end
+    it { expect(@image).to be_kind_of(Image) }
+    it { expect(@image.id).to eq(1) }
+  end
 end
